ac29afd31f2ff3d9f7ea856073e534c08f444b72
James Gomez <jgomez@progracol.com>
2024-03-19 07:49:04 -0500

    init commit

---
+++ src/main/java/com/delitech/revealing/RevealingpopayanApplication.java
@@ -0,0 +1,13 @@
+package com.delitech.revealing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevealingpopayanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevealingpopayanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/delitech/revealing/RevealingpopayanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delitech.revealing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevealingpopayanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
